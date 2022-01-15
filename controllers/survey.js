eba533cd4e957b8147714dd456abce18777b7c66
Bat-Ochir <baazka0705@gmail.com>
2022-01-15 10:38:29 +0800

    -survey 1 crud

--- controllers/survey.js
+++ controllers/survey.js
@@ -30,39 +30,49 @@ async function getSurvey1(req, res, next) {
 module.exports.getSurvey1 = getSurvey1;
 
 function getData(req) {
-  let letter = {
-    P_FAS_LETTER_DATA_ID: req.body.ID != null ? parseInt(req.body.ID) : null,
-    P_FAS_AUDIT_ID: parseInt(req.body.FAS_AUDIT_ID),
-    P_DOCUMENT_ID: parseInt(req.body.DOCUMENT_ID),
-    P_LETTER_TO: null,
-    P_REPORT_TYPE: null,
-    P_LAWS_NO: null,
-    P_BUDGET_TYPE: null,
-    P_LETTER_YEAR: null,
-    P_LETTER_MONTH: null,
-    P_LETTER_DAY: null,
-    P_REPORT_TYPE2: null,
-    P_REPORT_TYPE3: null,
-    P_DELIVERED_DATE:
-      req.body.DELIVERED_DATE != null
-        ? dateFormat(req.body.DELIVERED_DATE, "dd-mmm-yyyy")
-        : null,
-    P_LETTER_DESC: req.body.LETTER_DESC,
-    P_IS_ACTIVE: parseInt(req.body.IS_ACTIVE),
-    P_CREATED_BY: parseInt(req.body.CREATED_BY),
-    P_CREATED_DATE: dateFormat(req.body.CREATED_DATE, "dd-mmm-yyyy"),
+  let data = {
+    P_ID: req.body.ID != null ? parseInt(req.body.ID) : null,
+    SURVEY_ID: parseInt(req.body.SURVEY_ID),
+    HEMNELT_DUN: parseFloat(req.body.HEMNELT_DUN),
+    HEMNELT_PERCENT: parseFloat(HEMNELT_PERCENT),
+    NEW_EMP_COUNT: parseInt(req.body.NEW_EMP_COUNT),
+    NEW_EMP_SALARY: parseFloat(req.body.NEW_EMP_SALARY),
+    RENEW_EMP_COUNT: parseInt(req.body.RENEW_EMP_COUNT),
+    RENEW_EMP_SALARY: parseFloat(req.body.RENEW_EMP_SALARY),
+    IMP_SALARY_COUNT: parseInt(req.body.IMP_SALARY_COUNT),
+    IMP_SALARY_AMOUNT: parseFloat(req.body.IMP_SALARY_AMOUNT),
+    IMP_PROMO_COUNT: parseInt(req.body.IMP_PROMO_COUNT),
+    IMP_PROMO_AMOUNT: parseFloat(req.body.IMP_PROMO_AMOUNT),
+    IMP_GUEST_AMOUNT: parseFloat(req.body.IMP_GUEST_AMOUNT),
+    IMP_GIFT_AMOUNT: parseFloat(req.body.IMP_GIFT_AMOUNT),
+    CAR_COUNT: parseInt(req.body.CAR_COUNT),
+    CAR_AMOUNT: parseFloat(req.body.CAR_AMOUNT),
+    FURNITURE_AMOUNT: parseFloat(req.body.FURNITURE_AMOUNT),
+    CLOTHES_AMOUNT: parseFloat(req.body.CLOTHES_AMOUNT),
+    OTHER_TOOLS_AMOUNT: parseFloat(req.body.OTHER_TOOLS_AMOUNT),
+    EXTERNAL_EVENT_AMOUNT: parseFloat(req.body.EXTERNAL_EVENT_AMOUNT),
+    INTERNAL_EVENT_AMOUNT: parseFloat(req.body.INTERNAL_EVENT_AMOUNT),
+    CEREMONY_AMOUNT: parseFloat(req.body.CEREMONY_AMOUNT),
+    CONTENT_AMOUNT: parseFloat(req.body.CONTENT_AMOUNT),
+    BASE_PLAN_AMOUNT: parseFloat(req.body.BASE_PLAN_AMOUNT),
+    BASE_COMPLETION_AMOUNT: parseFloat(req.body.BASE_COMPLETION_AMOUNT),
+    BASE_PERCENT: parseFloat(req.body.BASE_PERCENT),
+    DOCUMENT_PLAN_AMOUNT: parseFloat(req.body.DOCUMENT_PLAN_AMOUNT),
+    DOCUMENT_COMPLETION_AMOUNT: parseFloat(req.body.DOCUMENT_COMPLETION_AMOUNT),
+    DOCUMENT_PERCENT: parseFloat(req.body.DOCUMENT_PERCENT),
+    CREATED_BY: parseInt(req.body.CREATED_BY),
   };
 
-  return letter;
+  return data;
 }
 
-async function postSurvey1Create(req, res, next) {
+async function postSurvey1CreateUpdate(req, res, next) {
   try {
     let data = getData(req);
     if (data.status !== "undefined" && data.status === "failed") {
       res.status(200).json({ message: "failed" });
     } else {
-      result = await survey.createSurvey1(data);
+      result = await survey.createUpdateSurvey1(data);
 
       res.status(200).json(result);
     }
@@ -71,4 +81,19 @@ async function postSurvey1Create(req, res, next) {
   }
 }
 
-module.exports.postSurvey1Create = postSurvey1Create;
+module.exports.postSurvey1CreateUpdate = postSurvey1CreateUpdate;
+
+async function postSurvey1Delete(req, res, next) {
+  try {
+    result = await survey.deleteSurvey1({
+      CREATED_BY: req.body.CREATED_BY,
+      P_ID: req.params.surveyid,
+    });
+
+    res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports.postSurvey1Delete = postSurvey1Delete;
--- services/router.js
+++ services/router.js
@@ -6,6 +6,7 @@ router.route("/survey/:yearid/:entid").get(survey.get);
 router
   .route("/survey1/:surveyid")
   .get(survey.getSurvey1)
-  .post(survey.postSurvey1Create);
+  .post(survey.postSurvey1CreateUpdate);
+router.route("/survey1Delete/:surveyid").post(survey.postSurvey1Delete);
 
 module.exports = router;
