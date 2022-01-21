dc074f9cd433c32bf6ea0fe31b9ae7a26a4752f8
Bat-Ochir <baazka0705@gmail.com>
2022-01-21 09:54:49 +0800

    -null survey1

--- controllers/survey.js
+++ controllers/survey.js
@@ -31,75 +31,114 @@ module.exports.getSurvey1 = getSurvey1;
 
 function getData(req) {
   let data = {};
-  if (req.body.ID != null)
-    data = {
-      P_ID: parseInt(req.body.ID),
-      HEMNELT_DUN: parseFloat(req.body.HEMNELT_DUN),
-      HEMNELT_PERCENT: parseFloat(req.body.HEMNELT_PERCENT),
-      NEW_EMP_COUNT: parseInt(req.body.NEW_EMP_COUNT),
-      NEW_EMP_SALARY: parseFloat(req.body.NEW_EMP_SALARY),
-      RENEW_EMP_COUNT: parseInt(req.body.RENEW_EMP_COUNT),
-      RENEW_EMP_SALARY: parseFloat(req.body.RENEW_EMP_SALARY),
-      IMP_SALARY_COUNT: parseInt(req.body.IMP_SALARY_COUNT),
-      IMP_SALARY_AMOUNT: parseFloat(req.body.IMP_SALARY_AMOUNT),
-      IMP_PROMO_COUNT: parseInt(req.body.IMP_PROMO_COUNT),
-      IMP_PROMO_AMOUNT: parseFloat(req.body.IMP_PROMO_AMOUNT),
-      IMP_GUEST_AMOUNT: parseFloat(req.body.IMP_GUEST_AMOUNT),
-      IMP_GIFT_AMOUNT: parseFloat(req.body.IMP_GIFT_AMOUNT),
-      CAR_COUNT: parseInt(req.body.CAR_COUNT),
-      CAR_AMOUNT: parseFloat(req.body.CAR_AMOUNT),
-      FURNITURE_AMOUNT: parseFloat(req.body.FURNITURE_AMOUNT),
-      CLOTHES_AMOUNT: parseFloat(req.body.CLOTHES_AMOUNT),
-      OTHER_TOOLS_AMOUNT: parseFloat(req.body.OTHER_TOOLS_AMOUNT),
-      EXTERNAL_EVENT_AMOUNT: parseFloat(req.body.EXTERNAL_EVENT_AMOUNT),
-      INTERNAL_EVENT_AMOUNT: parseFloat(req.body.INTERNAL_EVENT_AMOUNT),
-      CEREMONY_AMOUNT: parseFloat(req.body.CEREMONY_AMOUNT),
-      CONTENT_AMOUNT: parseFloat(req.body.CONTENT_AMOUNT),
-      BASE_PLAN_AMOUNT: parseFloat(req.body.BASE_PLAN_AMOUNT),
-      BASE_COMPLETION_AMOUNT: parseFloat(req.body.BASE_COMPLETION_AMOUNT),
-      BASE_PERCENT: parseFloat(req.body.BASE_PERCENT),
-      DOCUMENT_PLAN_AMOUNT: parseFloat(req.body.DOCUMENT_PLAN_AMOUNT),
-      DOCUMENT_COMPLETION_AMOUNT: parseFloat(
-        req.body.DOCUMENT_COMPLETION_AMOUNT
-      ),
-      DOCUMENT_PERCENT: parseFloat(req.body.DOCUMENT_PERCENT),
-      CREATED_BY: parseInt(req.body.CREATED_BY),
-    };
-  else
-    data = {
-      P_ID: null,
-      SURVEY_ID: parseInt(req.body.SURVEY_ID),
-      HEMNELT_DUN: parseFloat(req.body.HEMNELT_DUN),
-      HEMNELT_PERCENT: parseFloat(req.body.HEMNELT_PERCENT),
-      NEW_EMP_COUNT: parseInt(req.body.NEW_EMP_COUNT),
-      NEW_EMP_SALARY: parseFloat(req.body.NEW_EMP_SALARY),
-      RENEW_EMP_COUNT: parseInt(req.body.RENEW_EMP_COUNT),
-      RENEW_EMP_SALARY: parseFloat(req.body.RENEW_EMP_SALARY),
-      IMP_SALARY_COUNT: parseInt(req.body.IMP_SALARY_COUNT),
-      IMP_SALARY_AMOUNT: parseFloat(req.body.IMP_SALARY_AMOUNT),
-      IMP_PROMO_COUNT: parseInt(req.body.IMP_PROMO_COUNT),
-      IMP_PROMO_AMOUNT: parseFloat(req.body.IMP_PROMO_AMOUNT),
-      IMP_GUEST_AMOUNT: parseFloat(req.body.IMP_GUEST_AMOUNT),
-      IMP_GIFT_AMOUNT: parseFloat(req.body.IMP_GIFT_AMOUNT),
-      CAR_COUNT: parseInt(req.body.CAR_COUNT),
-      CAR_AMOUNT: parseFloat(req.body.CAR_AMOUNT),
-      FURNITURE_AMOUNT: parseFloat(req.body.FURNITURE_AMOUNT),
-      CLOTHES_AMOUNT: parseFloat(req.body.CLOTHES_AMOUNT),
-      OTHER_TOOLS_AMOUNT: parseFloat(req.body.OTHER_TOOLS_AMOUNT),
-      EXTERNAL_EVENT_AMOUNT: parseFloat(req.body.EXTERNAL_EVENT_AMOUNT),
-      INTERNAL_EVENT_AMOUNT: parseFloat(req.body.INTERNAL_EVENT_AMOUNT),
-      CEREMONY_AMOUNT: parseFloat(req.body.CEREMONY_AMOUNT),
-      CONTENT_AMOUNT: parseFloat(req.body.CONTENT_AMOUNT),
-      BASE_PLAN_AMOUNT: parseFloat(req.body.BASE_PLAN_AMOUNT),
-      BASE_COMPLETION_AMOUNT: parseFloat(req.body.BASE_COMPLETION_AMOUNT),
-      BASE_PERCENT: parseFloat(req.body.BASE_PERCENT),
-      DOCUMENT_PLAN_AMOUNT: parseFloat(req.body.DOCUMENT_PLAN_AMOUNT),
-      DOCUMENT_COMPLETION_AMOUNT: parseFloat(
-        req.body.DOCUMENT_COMPLETION_AMOUNT
-      ),
-      DOCUMENT_PERCENT: parseFloat(req.body.DOCUMENT_PERCENT),
-      CREATED_BY: parseInt(req.body.CREATED_BY),
-    };
+  data = {
+    P_ID: parseInt(req.body.ID),
+    HEMNELT_DUN:
+      req.body.HEMNELT_DUN != null ? parseFloat(req.body.HEMNELT_DUN) : null,
+    HEMNELT_PERCENT:
+      req.body.HEMNELT_PERCENT != null
+        ? parseFloat(req.body.HEMNELT_PERCENT)
+        : null,
+    NEW_EMP_COUNT:
+      req.body.NEW_EMP_COUNT != null ? parseInt(req.body.NEW_EMP_COUNT) : null,
+    NEW_EMP_SALARY:
+      req.body.NEW_EMP_SALARY != null
+        ? parseFloat(req.body.NEW_EMP_SALARY)
+        : null,
+    RENEW_EMP_COUNT:
+      req.body.RENEW_EMP_COUNT != null
+        ? parseInt(req.body.RENEW_EMP_COUNT)
+        : null,
+    RENEW_EMP_SALARY:
+      req.body.RENEW_EMP_SALARY != null
+        ? parseFloat(req.body.RENEW_EMP_SALARY)
+        : null,
+    IMP_SALARY_COUNT:
+      req.body.IMP_SALARY_COUNT != null
+        ? parseInt(req.body.IMP_SALARY_COUNT)
+        : null,
+    IMP_SALARY_AMOUNT:
+      req.body.IMP_SALARY_AMOUNT != null
+        ? parseFloat(req.body.IMP_SALARY_AMOUNT)
+        : null,
+    IMP_PROMO_COUNT:
+      req.body.IMP_PROMO_COUNT != null
+        ? parseInt(req.body.IMP_PROMO_COUNT)
+        : null,
+    IMP_PROMO_AMOUNT:
+      req.body.IMP_PROMO_AMOUNT != null
+        ? parseFloat(req.body.IMP_PROMO_AMOUNT)
+        : null,
+    IMP_GUEST_AMOUNT:
+      req.body.IMP_GUEST_AMOUNT != null
+        ? parseFloat(req.body.IMP_GUEST_AMOUNT)
+        : null,
+    IMP_GIFT_AMOUNT:
+      req.body.IMP_GIFT_AMOUNT != null
+        ? parseFloat(req.body.IMP_GIFT_AMOUNT)
+        : null,
+    CAR_COUNT: req.body.CAR_COUNT != null ? parseInt(req.body.CAR_COUNT) : null,
+    CAR_AMOUNT:
+      req.body.CAR_AMOUNT != null ? parseFloat(req.body.CAR_AMOUNT) : null,
+    FURNITURE_AMOUNT:
+      req.body.FURNITURE_AMOUNT != null
+        ? parseFloat(req.body.FURNITURE_AMOUNT)
+        : null,
+    CLOTHES_AMOUNT:
+      req.body.CLOTHES_AMOUNT != null
+        ? parseFloat(req.body.CLOTHES_AMOUNT)
+        : null,
+    OTHER_TOOLS_AMOUNT:
+      req.body.OTHER_TOOLS_AMOUNT != null
+        ? parseFloat(req.body.OTHER_TOOLS_AMOUNT)
+        : null,
+    EXTERNAL_EVENT_AMOUNT:
+      req.body.EXTERNAL_EVENT_AMOUNT != null
+        ? parseFloat(req.body.EXTERNAL_EVENT_AMOUNT)
+        : null,
+    INTERNAL_EVENT_AMOUNT:
+      req.body.INTERNAL_EVENT_AMOUNT != null
+        ? parseFloat(req.body.INTERNAL_EVENT_AMOUNT)
+        : null,
+    CEREMONY_AMOUNT:
+      req.body.CEREMONY_AMOUNT != null
+        ? parseFloat(req.body.CEREMONY_AMOUNT)
+        : null,
+    CONTENT_AMOUNT:
+      req.body.CONTENT_AMOUNT != null
+        ? parseFloat(req.body.CONTENT_AMOUNT)
+        : null,
+    BASE_PLAN_AMOUNT:
+      req.body.BASE_PLAN_AMOUNT != null
+        ? parseFloat(req.body.BASE_PLAN_AMOUNT)
+        : null,
+    BASE_COMPLETION_AMOUNT:
+      req.body.BASE_COMPLETION_AMOUNT != null
+        ? parseFloat(req.body.BASE_COMPLETION_AMOUNT)
+        : null,
+    BASE_PERCENT:
+      req.body.BASE_PERCENT != null ? parseFloat(req.body.BASE_PERCENT) : null,
+    DOCUMENT_PLAN_AMOUNT:
+      req.body.DOCUMENT_PLAN_AMOUNT != null
+        ? parseFloat(req.body.DOCUMENT_PLAN_AMOUNT)
+        : null,
+    DOCUMENT_COMPLETION_AMOUNT:
+      req.body.DOCUMENT_COMPLETION_AMOUNT != null
+        ? parseFloat(req.body.DOCUMENT_COMPLETION_AMOUNT)
+        : null,
+    DOCUMENT_PERCENT:
+      req.body.DOCUMENT_PERCENT != null
+        ? parseFloat(req.body.DOCUMENT_PERCENT)
+        : null,
+    CREATED_BY: parseInt(req.body.CREATED_BY),
+  };
+  if (req.body.ID != null) {
+    data.P_ID = req.body.ID;
+  } else {
+    data.P_ID = null;
+    data.SURVEY_ID = parseInt(req.body.SURVEY_ID);
+  }
+
   return data;
 }
 
