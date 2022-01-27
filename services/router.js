2f3e21f87bae3b342224b91b13c81f29dbe1f853
Uchralism <uchral.champ@gmail.com>
2022-01-27 12:15:11 +0800

    r

--- services/router.js
+++ services/router.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const { route } = require("express/lib/application");
 const router = new express.Router();
 const survey = require("../controllers/survey.js");
 
 router.route("/survey/:yearid/:entid").get(survey.get);
-router
-  .route("/survey1/:surveyid")
-  .get(survey.getSurvey1)
-  .post(survey.postSurvey1CreateUpdate);
+router.route("/survey1/:surveyid").get(survey.getSurvey1);
+router.route("/survey1Insert").post(survey.postSurvey1CreateUpdate);
 router.route("/survey1Delete/:surveyid").post(survey.postSurvey1Delete);
 
 module.exports = router;
